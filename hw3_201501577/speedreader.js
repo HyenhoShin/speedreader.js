4255f294c3c50c00893bc16ea0302689881dd5e8
HyenhoShin <50964779+HyenhoShin@users.noreply.github.com>
2019-05-24 06:16:14 +0900

    Add files via upload

---
+++ hw3_201501577/speedreader.js
@@ -0,0 +1,92 @@
+(function() {
+    "use strict";
+    
+    //alias to reduce typing
+    var $ = function(id) { return document.getElementById(id); };
+    var $$ = function(id) { return document.getElementsByName(id); };
+    
+    //global variables
+    var timer = null;
+    var output = [];
+    var index = 0;
+    
+    window.onload = function() {
+        //sets initial abilities for the controls
+        $("start").disabled = false; 
+        $("stop").disabled = true;
+        $("input").disabled = false;
+
+        $("start").onclick = createPassage;
+        $("input").onchange = changeText;
+        $("stop").onclick = clear;
+        for(var i = 0; i < $$("size").length; i++) {
+            $$("size")[i].onclick = editSize;
+        }
+        $("wordspeed").onchange = newTimer;
+    };
+    
+    function createPassage() {
+        var input = $("input").value;
+        if(output.length < 1) {
+            output = input.split(/[ \t\n]+/);
+        }
+        for(var i = 0; i < output.length; i++) {
+            if(output[i].endsWith(",")||output[i].endsWith(".")||output[i].endsWith("!")||
+               output[i].endsWith("?")||output[i].endsWith(":")||output[i].endsWith(";")) {
+                
+                output[i] = output[i].substring(0, output[i].length - 1);
+                output.splice(i, 0, output[i]);
+                i++;
+            }
+        }
+        createTimer();
+
+        $("start").disabled = true;
+        $("stop").disabled = false;
+        $("input").disabled = true;
+    }
+    
+    function changeText() {
+        output = [];
+    }
+    
+    function clear() {
+        $("reader").innerHTML = "";
+        index = 0;
+        clearInterval(timer);
+        
+        $("start").disabled = false;
+        $("stop").disabled = true;
+        $("input").disabled = false;
+    }
+    
+    function createTimer() {
+        var wordspeed = $("wordspeed");
+        var wpm = wordspeed.children[wordspeed.selectedIndex].value;
+        timer = setInterval(read, wpm);
+    }
+    
+    //Function that is called when the speed option is changed. Stops the current 
+    //timer and resets the timer to include the new speed.
+    function newTimer() {
+        if(!$("stop").disabled) {
+            clearInterval(timer);
+            createTimer();
+        }
+    }
+    
+    function read() {
+        var passage = $("reader");
+        if(output[index]) {
+            passage.innerHTML = output[index];
+            index++;
+        }else {
+            passage.innerHTML = "";
+            clear();
+        }
+    }
+
+    function editSize() {
+        $("reader").style.fontSize = this.value;
+    }
+})();
