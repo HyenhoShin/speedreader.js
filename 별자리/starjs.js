faec7ba83433c0931b820d3d26edda02a036a6d8
HyenhoShin <50964779+HyenhoShin@users.noreply.github.com>
2019-05-24 06:17:23 +0900

    Add files via upload

---
+++ 별자리/starjs.js
@@ -0,0 +1,18 @@
+﻿
+(function(){
+"use strict";    
+let today = new Date();
+let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let dateTime = date+' '+time;
+
+
+window.onload = function() {
+	
+	document.getElementById("time").innerHTML = dateTime;
+    };
+
+function clickFunction() {
+    alert("You clicked a tile!");
+}
+})();
